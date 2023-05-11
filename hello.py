95a5598b4a2e532b3fc934c4bf640e237f9ffbaa
Vassili Inacay <202110524@feualabang.edu.ph>
2023-05-11 18:28:34 +0800

    Added multiplication and division features. Created a new file.

--- hello.py
+++ hello.py
@@ -6,6 +6,10 @@ second_num = int(input("Enter second number: "))
 
 sum = first_num + second_num
 diff = first_num - second_num
+prod = first_num * second_num
+floor_div = first_num // second_num
 
 print(f"\n{first_num} + {second_num} = {sum}")
 print(f"{first_num} - {second_num} = {diff}")
+print(f"{first_num} * {second_num} = {prod}")
+print(f"{first_num} // {second_num} = {floor_div}")
