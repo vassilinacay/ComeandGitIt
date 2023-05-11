6611e8ce9ed2583c9aa92350e7936e882939d94b
Vassili Inacay <202110524@feualabang.edu.ph>
2023-05-11 18:05:33 +0800

    Added subtraction feature.

--- hello.py
+++ hello.py
@@ -5,5 +5,7 @@ first_num = int(input("Enter first number: "))
 second_num = int(input("Enter second number: "))
 
 sum = first_num + second_num
+diff = first_num - second_num
 
 print(f"\n{first_num} + {second_num} = {sum}")
+print(f"{first_num} - {second_num} = {diff}")
