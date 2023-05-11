9ae88ea2a25115f56efb8b056f80d5722f683478
Vassili Inacay <202110524@feualabang.edu.ph>
2023-05-11 17:53:51 +0800

    Initial Commit
    This is my first ever commit in Git.

---
+++ hello.py
@@ -0,0 +1,9 @@
+
+print("hello world!")
+
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+
+sum = first_num + second_num
+
+print(f"\n{first_num} + {second_num} = {sum}")
