8a25a5af24e7a51e4e5008b5cdf1fb96baa8c1af
Vassili Inacay <202110524@feualabang.edu.ph>
2023-05-11 18:58:40 +0800

    Added modulo feature.

--- hello.py
+++ hello.py
@@ -1,15 +1,16 @@
+print("\nCALCULATOR IN PYTHON")
 
-print("hello world!")
-
-first_num = int(input("Enter first number: "))
+first_num = int(input("\nEnter first number: "))
 second_num = int(input("Enter second number: "))
 
 sum = first_num + second_num
 diff = first_num - second_num
 prod = first_num * second_num
 floor_div = first_num // second_num
+mod = first_num % second_num
 
 print(f"\n{first_num} + {second_num} = {sum}")
 print(f"{first_num} - {second_num} = {diff}")
 print(f"{first_num} * {second_num} = {prod}")
 print(f"{first_num} // {second_num} = {floor_div}")
+print(f"{first_num} % {second_num} = {mod}")
